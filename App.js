6d596cb8f7992ef94c1a97826b23821cc5c87b52
Bui Khiem <1459025@itec.hcmus.edu.vn>
2017-10-21 10:55:27 +0700

    fixing bugs

--- App.js
+++ App.js
@@ -5,47 +5,14 @@ import { AppRegistry, View, Text, StyleSheet } from 'react-native'
 // import MyScrollView from './components/scrollview'
 // import DisplayFlatList from './components/flatlist'
 // import DisplaySectionList from './components/sectionlist'
-import List from './components/List'
-import Input from './components/Input'
-import Title from './components/Title'
+import Home from './components/Home'
+
 
 export default class App extends Component { 
-  state = {
-    participants:[
-      'Bui Quoc Khiem',
-      'Pham Tran Tuan Dat',
-      'Nguyen Pham Huy Khiem',
-      'Tran Duc Vinh',
-       'Tran Ngoc Phu'
-    ]
-  }
-  onAddParticipant = (text)=>{
-    const {participants} = this.state;
-    this.setState({
-      participants:[text,...participants],
-    })
-  }
-  onRemoveParticipant = (index)=>{
-    const {participants} = this.state;
-    this.setState({
-      participants: participants.filter((participant,i)=>i!==index),
-    })
-  }
     render(){
-      const {participants} = this.state;
       return(
         <View>
-          <Title>
-            Chat participants
-          </Title>
-          <Input 
-          placeholder={'type a name and hit enter'}
-          onSubmitEditing={this.onAddParticipant}
-          />
-          <List
-            list={participants}
-            onPressItem={this.onRemoveParticipant}
-          />
+          <Home/>
         </View>
       )
     } 
---
+++ components/Home.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { AppRegistry, View, Text, StyleSheet } from 'react-native'
+// import DisplayCounter from './components/counter'
+// import DisplayToggle from './components/toggle'
+// import MyScrollView from './components/scrollview'
+// import DisplayFlatList from './components/flatlist'
+// import DisplaySectionList from './components/sectionlist'
+import List from './List'
+import Input from './Input'
+import Title from './Title'
+
+export default class Home extends Component { 
+  state = {
+    participants:[
+      'Bui Quoc Khiem',
+      'Pham Tran Tuan Dat',
+      'Nguyen Pham Huy Khiem',
+      'Tran Duc Vinh',
+       'Tran Ngoc Phu'
+    ]
+  }
+  onAddParticipant = (text)=>{
+    const {participants} = this.state;
+    this.setState({
+      participants:[text,...participants],
+    })
+  }
+  onRemoveParticipant = (index)=>{
+    const {participants} = this.state;
+    this.setState({
+      participants: participants.filter((participant,i)=>i!==index),
+    })
+  }
+    render(){
+      const {participants} = this.state;
+      return(
+        <View>
+          <Title>
+            Chat participants
+          </Title>
+          <Input 
+          placeholder={'type a name and hit enter'}
+          onSubmitEditing={this.onAddParticipant}
+          />
+          <List
+            list={participants}
+            onPressItem={this.onRemoveParticipant}
+          />
+        </View>
+      )
+    } 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+// AppRegistry.registerComponent('App', () => App)
