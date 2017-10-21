82fe132e86ba1d1367c7baaee2c5ce54d3ae12c7
Bui Khiem <1459025@itec.hcmus.edu.vn>
2017-10-21 10:41:43 +0700

    input text and remove text

--- App.js
+++ App.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react'
 import { AppRegistry, View, Text, StyleSheet } from 'react-native'
-import DisplayCounter from './components/counter'
-import DisplayToggle from './components/toggle'
-import MyScrollView from './components/scrollview'
-import DisplayFlatList from './components/flatlist'
-import DisplaySectionList from './components/sectionlist'
+// import DisplayCounter from './components/counter'
+// import DisplayToggle from './components/toggle'
+// import MyScrollView from './components/scrollview'
+// import DisplayFlatList from './components/flatlist'
+// import DisplaySectionList from './components/sectionlist'
+import List from './components/List'
+import Input from './components/Input'
+import Title from './components/Title'
 
 export default class App extends Component { 
-  render() {
-    return (
-      <View style={styles.container}>
-        {/* <DisplayCounter/> */}
-        {/* <DisplayToggle/> */}
-        {/* <MyScrollView/> */}
-        {/* <DisplayFlatList/> */}
-        <DisplaySectionList/>
-      </View>
-    )
+  state = {
+    participants:[
+      'Bui Quoc Khiem',
+      'Pham Tran Tuan Dat',
+      'Nguyen Pham Huy Khiem',
+      'Tran Duc Vinh',
+       'Tran Ngoc Phu'
+    ]
   }
+  onAddParticipant = (text)=>{
+    const {participants} = this.state;
+    this.setState({
+      participants:[text,...participants],
+    })
+  }
+  onRemoveParticipant = (index)=>{
+    const {participants} = this.state;
+    this.setState({
+      participants: participants.filter((participant,i)=>i!==index),
+    })
+  }
+    render(){
+      const {participants} = this.state;
+      return(
+        <View>
+          <Title>
+            Chat participants
+          </Title>
+          <Input 
+          placeholder={'type a name and hit enter'}
+          onSubmitEditing={this.onAddParticipant}
+          />
+          <List
+            list={participants}
+            onPressItem={this.onRemoveParticipant}
+          />
+        </View>
+      )
+    } 
 }
 
 const styles = StyleSheet.create({
